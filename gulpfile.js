16e24a965ec2ab1d3b275aa6f9d56788460b3c1e
jean carllo <jeanrosete04@gmail.com>
2023-11-21 14:48:22 -0300

    criando o hero e importando a font Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
-    return gulp.src('./src/styles/*scss')
+    return gulp.src('./src/styles/**/*scss')
     .pipe(sass({outputStyle: 'compressed' }))
-    .pipe(gulp.dest('./dist/css'))
+    .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function image() {
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, image);
 exports.watch = function() {
     gulp.watch('./src/styles/*scss', gulp.parallel(styles))
 }
